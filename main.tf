b6eaecdbef621988e1537a9ec616cb72d7fd051a
akhil kumar <akhil@gmail.com>
2023-10-12 01:29:29 +0530

    moving files

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_security_group" "sg" {
-  name        = "${var.name}-${var.env}"
+  name        = "${var.name}-${var.env}-sg"
   description = "${var.name}-${var.env}-sg"
   vpc_id      = var.vpc_id
 
