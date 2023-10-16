d76a7a8378d18b2e3f38592f2941ed9715b3c0e3
akhil kumar <akhil@gmail.com>
2023-10-16 15:04:46 +0530

    moving files

--- main.tf
+++ main.tf
@@ -48,6 +48,7 @@ resource "aws_autoscaling_group" "asg" {
   max_size           = var.max_size
   min_size           = var.min_size
   vpc_zone_identifier = var.subnet_ids
+  target_group_arns  = [aws_lb_target_group.main.arn]
 
   launch_template {
     id      = aws_launch_template.template.id
@@ -62,4 +63,11 @@ resource "aws_autoscaling_group" "asg" {
       value               = tag.value
     }
   }
+}
+
+resource "aws_lb_target_group" "main" {
+  name     = "tf-example-lb-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
 }
