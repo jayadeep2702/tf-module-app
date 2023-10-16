c96a699c85a4362b47f078d0b1ca2d13bf85f1fd
akhil kumar <akhil@gmail.com>
2023-10-16 19:12:59 +0530

    moving files

--- main.tf
+++ main.tf
@@ -66,7 +66,7 @@ resource "aws_autoscaling_group" "asg" {
 }
 
 resource "aws_lb_target_group" "main" {
-  name     = "${var.name}-${var.env}-asg"
+  name     = "${var.name}-${var.env}-tg"
   port     = var.app_port
   protocol = "HTTP"
   vpc_id   = var.vpc_id
