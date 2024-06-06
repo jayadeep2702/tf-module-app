ce18e9c222753e92d078ecc39776a6b4c649b71d
akhil kumar <akhil@gmail.com>
2024-06-06 12:44:41 +0530

    moving files

--- userdata.sh
+++ userdata.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 labauto ansible
+labauto python
 ansible-pull -i localhost, -U https://github.com/jayadeep2702/raghu-ansible-roboshop roboshop.yaml -e role_name=${name} -e env=${env} &>>/opt/ansible.log
