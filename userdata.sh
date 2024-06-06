2b75c8110c79c2d2bab3bd3d9b64e57379e95158
akhil kumar <akhil@gmail.com>
2024-06-06 18:10:42 +0530

    moving files

--- userdata.sh
+++ userdata.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-yum install ansible -y
+yum install ansible python3.12-pip.noarch -y &>>/opt/ansible.log
+pip3.12 install botocore boto3
 ansible-pull -i localhost, -U https://github.com/jayadeep2702/raghu-ansible-roboshop roboshop.yaml -e role_name=${name} -e env=${env} &>>/opt/ansible.log
